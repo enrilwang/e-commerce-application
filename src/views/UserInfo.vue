772c5531f8116345cffedea368aa9b5d8bbd412b
shxu3818 <shxu3818@uni.sydney.edu.au>
2021-05-22 15:50:28 +0800

    111

--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -26,32 +26,57 @@
 
             <el-tab-pane label="Manage listings">
               <el-button type="text" @click="add()">Add a new list</el-button>
-              <div class="form1" v-show="formState">
-              <el-form ref="form" :model="form" label-width="120px"  >
-                <el-form-item label="Title">
-                  <el-input v-model="form.title"></el-input>
-                </el-form-item>
-                <el-form-item label="Brand">
-                  <el-input v-model="form.brand"></el-input>
-                </el-form-item>
-                <el-form-item label="Image">
-                  <el-input v-model="form.image"></el-input>
-                </el-form-item>
-                <el-form-item label="Stock">
-                  <el-input v-model="form.stock"></el-input>
-                </el-form-item>
-                <el-form-item label="Price">
-                  <el-input v-model="form.price"></el-input>
-                </el-form-item>
-                <el-form-item label="Disabled">
-                  <el-switch v-model="form.disabled"></el-switch>
-                </el-form-item>
-                <el-form-item>
-                  <el-button type="primary" @click="add1()">Add a new list</el-button>
-                  <el-button @click="cancel()">Cancel</el-button>
-                </el-form-item>
-              </el-form>
+              <div class="container">
+                <div class="form1" v-show="formState">
+                <el-form ref="form" :model="form" label-width="120px"  >
+                  <el-form-item label="Title">
+                    <el-input v-model="form.title"></el-input>
+                  </el-form-item>
+                  <el-form-item label="Brand">
+                    <el-input v-model="form.brand"></el-input>
+                  </el-form-item>
+                  <el-form-item label="Image">
+                    <el-input v-model="form.image"></el-input>
+                  </el-form-item>
+                  <el-form-item label="Stock">
+                    <el-input v-model="form.stock"></el-input>
+                  </el-form-item>
+                  <el-form-item label="Price">
+                    <el-input v-model="form.price"></el-input>
+                  </el-form-item>
+                  <el-form-item label="Disabled">
+                    <el-switch v-model="form.disabled"></el-switch>
+                  </el-form-item>
+                  <el-form-item>
+                    <el-button type="primary" @click="add1()">Add a new list</el-button>
+                    <el-button @click="cancel()">Cancel</el-button>
+                  </el-form-item>
+                </el-form>
+                </div>
               </div>
+              <div class="disable" v-show="disableState">
+                <el-switch v-model="enable"></el-switch>
+
+              </div>
+               <el-row>
+                  <el-col :span="6" v-for="(post,index) in addedList" 
+                    v-bind:item="post"
+                    v-bind:index="index"
+                    v-bind:key="post.id" >
+                    <!-- <el-switch v-model="form.disabled"></el-switch> -->
+
+                    <el-card :body-style="{ padding: '0px' }">
+                      <div style="padding: 14px;">  
+                        <img :src="require('../../public/image/'+post.image)" style="width:100%" class="image"  alt="">
+                        <!-- <span><h3>Image:</h3> ../../public/image/{{post.image}}</span><br> -->
+                        <span><h3>Title:</h3> {{post.title}}</span>
+                        <span><h3>Brand:</h3> {{post.brand}}</span>
+                        <span><h3>Stock:</h3> {{post.stock}}</span>
+                        <span><h3>Price:</h3> ${{post.price}}</span>
+                      </div>
+                    </el-card>
+                  </el-col>
+                </el-row> 
             </el-tab-pane>
             </el-tabs>
         </div>
@@ -72,6 +97,8 @@ export default {
         newpwd:'',
         id:"",
         addedList:[],
+        disableState:false,
+        enable:true,
         
         form: {
           title: '',
@@ -205,8 +232,12 @@ export default {
              axios.get("http://localhost:3000/userAddList",{headers:{"Content-Type":"application/json"}})
             .then(res =>{
               this.addedList = res.data 
+              console.log(this.addedList)
               
             })
+            this.formState=false
+            this.disableState=true
+            
             alert("Added successfully")
             
           }else if(res.status ===201){
@@ -284,5 +315,11 @@ margin-top: 0px
 }
 .user input[type=text] {
   width: 100%;
+}
+.el-card {
+  height: 600px;
+  margin-left: 36px;
+  margin-top: 15px;
+  
 }
 </style>
